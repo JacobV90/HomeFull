f0b5035df5017db9aaab95595b448bdb999ae209
Jacob Veal <jacob-veal@uiowa.edu>
2016-10-23 00:26:57 -0500

    user details page updated

--- client/src/controllers/controllers.ts
+++ client/src/controllers/controllers.ts
@@ -1,38 +1,113 @@
-angular.module('app.controllers', [ 'ionic', 'firebase'])
+angular.module('app.controllers', [ 'ionic', 'firebase', 'ngAnimate'])
 
   .controller('ShelterCtrl', function($scope, $ionicModal, $firebaseArray) {
 
-    //var shelters = Shelters.all();
+  //var shelters = Shelters.all();
 
   var ref = firebase.database().ref().child('shelters');
   $scope.shelters = $firebaseArray(ref);
+  $scope.userData = {};
+  var emergency = false;
+  var alone = false;
+  var group = false;
+  $scope.categories = [
+    {
+      name: "Life threatening?",
+      value: emergency,
+      label: "emergency"
+    },
+    {
+      name: "Are you alone?",
+      value: alone,
+      label: "alone"
+    },
+    {
+      name: "In a group",
+      value: group,
+      label: "group"
+    }
+  ]
 
   $ionicModal.fromTemplateUrl('templates/shelter-details.html',{
     scope: $scope,
     animation: 'slide-in-up'
   }).then(function(modal) {
-    $scope.modal = modal;
+    $scope.shelterModal = modal;
   });
 
-  $scope.openModal = function(shelter) {
-    $scope.modalData = {"name": shelter.name, "address": shelter.address};
+  $ionicModal.fromTemplateUrl('templates/user-details.html',{
+    scope: $scope,
+    animation: 'slide-in-down'
+  }).then(function(modal) {
+    $scope.userModal = modal;
+  });
+
+  $scope.openShelter = function(shelter) {
+    $scope.shelter = {"name": shelter.name, "address": shelter.address};
+    $scope.shelterModal.show();
+  };
+
+  $scope.openUser = function() {
+    $scope.userModal.show();
+  };
+
+  $scope.hideShelter = function(){
+    $scope.shelterModal.hide();
+  };
+
+  $scope.hideUser = function(){
+    $scope.userModal.hide();
+    console.log($scope.emergency);
+  };
+
+  $scope.emergency = function(){
+    $scope.alone = !$scope.alone;
+    $scope.group = !$scope.alone;
+  };
+
+  $scope.toggleCategory = function(category) {
+    var theone = category.label;
+    switch(theone){
+      case "emergency":
+          alone = !alone;
+          group = !group;
+          $scope.categories[1].value = alone;
+          $scope.categories[2].value = group;
+          break;
+      case "alone":
+          emergency = !emergency;
+          group = !group;
+          $scope.categories[0].value = emergency;
+          $scope.categories[2].value = group;
+          break;
+      case "group":
+          emergency = !emergency;
+          alone = !alone;
+          $scope.categories[0].value = emergency;
+          $scope.categories[1].value = alone;
+          break;
+    }
+
+    if ($scope.isCategoryShown(category)) {
+      $scope.shownCategory = null;
+    } else {
+      $scope.shownCategory = category;
+    }
+  };
+
+  $scope.isCategoryShown = function(category) {
+    return $scope.shownCategory === category;
+  };
+
+})
+.controller('FormCtrl', function($scope, $ionicModal){
+
+  $scope.openForm = function() {
     $scope.modal.show();
   };
 
-  $scope.hideModal = function(){
+  $scope.closeForm = function(){
     $scope.modal.hide();
-  }
+  };
 
-  // Cleanup the modal when we're done with it!
-  $scope.$on('$destroy', function() {
-    $scope.modal.remove();
-  });
-  // Execute action on hide modal
-  $scope.$on('modal.hidden', function() {
-    // Execute action
-  });
-  // Execute action on remove modal
-  $scope.$on('modal.removed', function() {
-    // Execute action
-  });
 });
---
+++ client/src/services/services.ts
@@ -0,0 +1,40 @@
+/*angular.module('services')
+
+.factory('Shelters', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var shelters = [{
+    id: 0,
+    name: 'St. Patrick Center',
+    address: '800 N Tucker Blvd St. Louis, MO 63101',
+    availability: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Covenant House',
+    address: '2727 N Kingshighway Blvd, St. Louis, MO 63113',
+    availability: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Almost Home',
+    address: '3200 St Vincent Ave, St. Louis, MO 63104',
+    availability: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'St Peter & Paul Shelter',
+    address: '711 Allen Ave, St. Louis, MO 63104',
+    availability: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Our Ladys Inn',
+    address: '4223 S Compton Ave, St. Louis, MO',
+    availability: 'img/mike.png'
+  }];
+
+  return {
+    all: function() {
+      return shelters;
+    }
+
+  };
+});*/
