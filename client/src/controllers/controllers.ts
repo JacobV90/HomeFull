808a30fa7ea07941493d203fdb3d8e70a5dde670
Jacob Veal <jacob-veal@uiowa.edu>
2016-10-23 06:36:20 -0500

    map progress

--- client/src/controllers/controllers.ts
+++ client/src/controllers/controllers.ts
@@ -123,4 +123,87 @@ angular.module('app.controllers', [ 'ionic', 'firebase', 'ngAnimate'])
     $scope.modal.hide();
   };
 
+})
+.controller('GeoCtrl', function($cordovaGeolocation) {
+
+  var lat = null;
+  var long = null;
+
+  var posOptions = {timeout: 10000, enableHighAccuracy: false};
+  $cordovaGeolocation
+    .getCurrentPosition(posOptions)
+    .then(function (position) {
+      lat  = position.coords.latitude
+      long = position.coords.longitude
+    }, function(err) {
+      // error
+    });
+
+
+  var watchOptions = {
+    timeout : 3000,
+    enableHighAccuracy: false // may cause errors if true
+  };
+
+  var watch = $cordovaGeolocation.watchPosition(watchOptions);
+  watch.then(
+    null,
+    function(err) {
+      // error
+    },
+    function(position) {
+      lat  = position.coords.latitude
+      long = position.coords.longitude
+  });
+
+  function calculateDistance(lat2, lon2) {
+    var R = 6371; // km
+    var dLat = toRad(lat2 - lat);
+    var dLon = toRad(lon2 - long);
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(lat.toRad()) * Math.cos(lat2.toRad()) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d;
+  }
+  function toRad(value) {
+    return value * Math.PI / 180;
+  }
+
+
+  watch.clearWatch();
+  // OR
+  $cordovaGeolocation.clearWatch(watch)
+    .then(function(result) {
+      // success
+      }, function (error) {
+      // error
+    });
+})
+.controller('MapController', function($scope, $cordovaGeolocation, $ionicPlatform ,$ionicLoading) {
+
+    $ionicPlatform.ready(function(){
+      $ionicLoading.show({
+                  template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
+              });
+
+              var options = {timeout: 10000, enableHighAccuracy: true};
+
+      $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+
+        var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+        var mapOptions = {
+          center: latLng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+      }, function(error){
+        console.log("Could not get location");
+      });
+    })
 });
